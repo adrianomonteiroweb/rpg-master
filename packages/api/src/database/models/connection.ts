5845df7a0e71b5c7ab4a8678251c67a14f9c290c
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2023-05-10 18:31:35 -0300

    new: connection and compaign.model files for database using database

--- packages/api/src/database/models/connection.ts
+++ packages/api/src/database/models/connection.ts
@@ -1,18 +1,18 @@
 import { connect } from "mongoose";
 
-import CampaignModel from "./campaigns/compaigns.model";
+import CampaignModel from "./compaigns.model";
 
 async function runConnect() {
   await connect("mongodb+srv://adrianomonteirodev:adrianomonteirodev@cluster0.b2gy5pv.mongodb.net/?retryWrites=true&w=majority");
 
-  const campaign = new CampaignModel({
-    title: "Assaid",
-    subtitle: "Entre..."
-  });
+  // const campaign = new CampaignModel({
+  //   title: "Assaid",
+  //   subtitle: "Entre..."
+  // });
 
-  await campaign.save();
+  // await campaign.save();
 
-  console.log(campaign);
+  // console.log(campaign);
   
 }
 
