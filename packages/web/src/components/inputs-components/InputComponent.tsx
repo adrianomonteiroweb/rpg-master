3bb6f7f0f7b9b272d81be12dc994cd7b1a475afe
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2023-05-11 10:08:13 -0300

    new: root screen with campaign session

---
+++ packages/web/src/components/inputs-components/InputComponent.tsx
@@ -0,0 +1,41 @@
+import Form from "react-bootstrap/Form";
+import PropTypes from "prop-types";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./inputComponent.css";
+
+interface IInput {
+  input: any
+}
+
+function InputComponent({
+  input: [
+    type,
+    id,
+    value,
+    nivelText,
+    eventFunc,
+    prop,
+    nivel ]
+}: IInput) {
+  return (
+    <>
+      <Form.Label htmlFor={`${id}`}>
+        <h6>{`${nivelText}`}</h6>
+      </Form.Label>
+      <Form.Control
+        type={`${type}`}
+        className="input-component"
+        id={`${id}`}
+        value={value}
+        onChange={(event) => eventFunc ? eventFunc(event, prop, nivel) : undefined}
+      />
+    </>
+  );
+}
+
+InputComponent.propTypes = {
+  input: PropTypes,
+};
+
+export default InputComponent;
