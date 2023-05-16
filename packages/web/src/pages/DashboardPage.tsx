9f214ec4a164f2a82b4fc0315606d73238e4f844
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2023-05-16 13:33:27 -0300

    new: show and hide method for characters session

--- packages/web/src/pages/DashboardPage.tsx
+++ packages/web/src/pages/DashboardPage.tsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
+import ButtonComponent from "../components/button-component/ButtonComponent";
 import CampaignSession from "../components/sessions-components/CampaignSession";
 import CharacterSession from "../components/sessions-components/CharacterSession";
 
 import "./dashboardPage.css"
 
 function Dashboard() {
-    return (
-      <div className="dashboard">
-        <CampaignSession />
-        <CharacterSession />
-      </div>
-    );
-  }
-  
-  export default Dashboard;
+  const [showCharacters, setShowCharacters] = useState(false);
+
+  const changeShowCharacters = () => !showCharacters ? setShowCharacters(true) : setShowCharacters(false);
+
+  return (
+    <div className="dashboard">
+      <CampaignSession />
+      <ButtonComponent
+        btn={[
+          !showCharacters ? "+ Exibir Sessão de Personagens" : "- Esconder Sessão de Personagens",
+          "show-hide-button",
+          "show-hide-characters",
+          changeShowCharacters
+        ]}
+      />
+      {!showCharacters ? undefined : <CharacterSession />}
+    </div>
+  );
+}
+
+export default Dashboard;
